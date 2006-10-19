af61d863aca5c35174731764d2ab35263ca2da28
Sylvain Beucler <beuc@beuc.net>
2006-10-19 12:32:03 +0200

    Imported Debian patch 1.6.7-2~bpo.1

--- base/g.py
+++ base/g.py
@@ -38,7 +38,7 @@ log = logger.Logger('', logger.Logger.LOG_LEVEL_INFO, logger.Logger.LOG_TO_CONSO
 log.set_level('info')
 
 MINIMUM_PYQT_MAJOR_VER = 3
-MINIMUM_PYQT_MINOR_VER = 14
+MINIMUM_PYQT_MINOR_VER = 13
 MINIMUM_QT_MAJOR_VER = 3
 MINIMUM_QT_MINOR_VER = 0
 
