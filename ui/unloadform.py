3a02056ce52f22480a87a861f20dc7e7ccce27aa
Till Kamppeter <till.kamppeter@gmail.com>
2018-10-03 15:13:00 +0200

    Call QMessageBox constructors of PyQT5 with the correct parameters
    The whole Python source code of HPLIP contains 40 incorrect calls of
    the QMessageBox constructurs, mostly the list of buttons was given as
    separate arguments instead of a bitwise OR of each button's
    representation value.  If the flow of the code hits one of these
    calls (usually when some error or warnings message should be
    displayed or an "Are you sure?" question asked) the GUI app crashes.
    See https://pythonspot.com/pyqt5-messagebox/ for QMessageBox
    documentation.
    This patch corrects all these calls.
    
    Bug-Debian: https://bugs.debian.org/912768
    Bug-Ubuntu: https://bugs.launchpad.net/bugs/1745383

--- ui/unloadform.py
+++ ui/unloadform.py
@@ -135,7 +135,7 @@ class UnloadForm(QMainWindow):
         QMessageBox.critical(self,
                              self.caption(),
                              error_text,
-                              QMessageBox.Ok,
+                              QMessageBox.Ok |\
                               QMessageBox.NoButton,
                               QMessageBox.NoButton)
 
